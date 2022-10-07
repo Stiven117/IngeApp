30384df0887814b993b63aadf217088c3d758910
Stiven117 <stiventoledob@gmail.com>
2022-10-07 13:05:37 -0500

    Prueba GIT

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Ingenio Prueba</h1>
+          <h1>Ingenio Prueba GIT</h1>
         </p>
         <a
           className="App-link"
